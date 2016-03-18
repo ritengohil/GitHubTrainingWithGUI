c61c12f44484762a37c4bdc45c8dd3f7851c5593
ritengohil <rgohil@webuy.com>
2016-03-18 13:55:57 +0530

    First commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+SELECT name AS object_name 
+  ,SCHEMA_NAME(schema_id) AS schema_name
+  ,type_desc
+  ,create_date
+  ,modify_date
+  ,object_id
+FROM sys.objects
+WHERE modify_date > GETDATE() - 10
+ORDER BY modify_date; 
